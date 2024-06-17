ac34f6d1a21f27f30e28b2f386ae810239abf4de
Mike Attara <mpyebattara@gmail.com>
2024-06-17 02:57:28 +0000

    basic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+number1 = 7
+number2 = 8
+
+# Addition
+print('Addition of ', number1, ' and ',number2, ' is ', number1 + number2)
+
+# Subtraction
+print('Subtraction of ', number1, ' and ',number2, ' is ', number1 - number2)
+
+# Multiplication
+print('Multiplication of ', number1, ' and ',number2, ' is ', number1 * number2)
