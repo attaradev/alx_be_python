a483f7dbc346e371fe2f3bd34f8c2e9a48b9237f
Mike Attara <mpyebattara@gmail.com>
2024-07-08 14:29:35 +0000

    add bank account

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,15 @@
+class BankAccount:
+    def __init__(self, balance=0.0):
+        self.account_balance = balance
+
+    def deposit(self, amount):
+        self.account_balance += amount
+
+    def withdraw(self, amount):
+        if self.account_balance < amount:
+            return False
+        self.account_balance -= amount
+        return True
+
+    def display_balance(self):
+        print(f"Account balance: ${self.account_balance:.2f}")
